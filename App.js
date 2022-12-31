c0d48e1176abbf03dc5d1d7ff0933d85c5a296b0
Durga Nepal <durgain.np@gmail.com>
2023-01-01 08:33:27 +0900

    practice with flatList

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import MenuItems from "./components/MenuItems";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "space-between",
+        backgroundColor: "#495E57",
+      }}
+    >
+      <LittleLemonHeader />
+      <MenuItems />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/LittleLemonHeader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function LittleLemonHeader() {
+  return (
+    <View
+      style={{
+        flex: 0.2,
+        backgroundColor: "#F4CE14",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 25,
+          color: "#000000",
+          alignSelf: "center",
+        }}
+      >
+        LITTLE LEMON
+      </Text>
+    </View>
+  );
+}
