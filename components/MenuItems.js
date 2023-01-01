0e8ed8e8dd4ba55ed219de100a87228be255ccec
Durga Nepal <durgain.np@gmail.com>
2023-01-01 10:07:51 +0900

    Render Menu item with SectionList component

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,38 +1,53 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SectionList, StyleSheet, Text, View } from "react-native";
 
 export default function MenuItems() {
   const menuItemsToDisplay = [
-    { name: "Hummus", price: "$5.00", id: "1A" },
-    { name: "Moutabal", price: "$5.00", id: "2B" },
-    { name: "Falafel", price: "$7.50", id: "3C" },
-    { name: "Marinated Olives", price: "$5.00", id: "4D" },
-    { name: "Kofta", price: "$5.00", id: "5E" },
-    { name: "Eggplant Salad", price: "$8.50", id: "6F" },
-    { name: "Lentil Burger", price: "$10.00", id: "7G" },
-    { name: "Smoked Salmon", price: "$14.00", id: "8H" },
-    { name: "Kofta Burger", price: "$11.00", id: "9I" },
-    { name: "Turkish Kebab", price: "$15.50", id: "10J" },
-    { name: "Fries", price: "$3.00", id: "11K" },
-    { name: "Buttered Rice", price: "$3.00", id: "12L" },
-    { name: "Bread Sticks", price: "$3.00", id: "13M" },
-    { name: "Pita Pocket", price: "$3.00", id: "14N" },
-    { name: "Lentil Soup", price: "$3.75", id: "15O" },
-    { name: "Greek Salad", price: "$6.00", id: "16Q" },
-    { name: "Rice Pilaf", price: "$4.00", id: "17R" },
-    { name: "Baklava", price: "$3.00", id: "18S" },
-    { name: "Tartufo", price: "$3.00", id: "19T" },
-    { name: "Tiramisu", price: "$5.00", id: "20U" },
-    { name: "Panna Cotta", price: "$5.00", id: "21V" },
+    {
+      title: "Appetizers",
+      data: [
+        "Hummus",
+        "Moutabal",
+        "Falafel",
+        "Marinated Olives",
+        "Kofta",
+        "Eggplant Salad",
+      ],
+    },
+    {
+      title: "Main Dishes",
+      data: ["Lentil Burger", "Smoked Salmon", "Kofta Burger", "Turkish Kebab"],
+    },
+    {
+      title: "Sides",
+      data: [
+        "Fries",
+        "Buttered Rice",
+        "Bread Sticks",
+        "Pita Pocket",
+        "Lentil Soup",
+        "Greek Salad",
+        "Rice Pilaf",
+      ],
+    },
+    {
+      title: "Desserts",
+      data: ["Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
+    },
   ];
-  const Item = ({ name, price }) => (
+
+  const Item = ({ name }) => (
     <View style={menuStyles.menuItem}>
       <Text style={menuStyles.itemText}>{name}</Text>
-      <Text style={menuStyles.itemText}>{price}</Text>
     </View>
   );
+  const renderItem = ({ item }) => <Item name={item} />;
+  const renderSectionHeader = ({ section }) => (
+    <Text style={menuStyles.sectionHeader}>{section.title}</Text>
+  );
+
   const Header = () => <Text style={menuStyles.headerText}>View Menu</Text>;
-  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
+  const Saperator = () => <View style={menuStyles.saperator}></View>;
   const Footer = () => (
     <Text style={menuStyles.footerStyle}>
       {"\u00a9"} All rights reserved by Little Lemon,
@@ -42,14 +57,15 @@ export default function MenuItems() {
 
   return (
     <View style={menuStyles.container}>
-      <FlatList
-        data={menuItemsToDisplay}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
+      <SectionList
+        keyExtractor={(item, index) => item + index}
+        sections={menuItemsToDisplay}
         ListHeaderComponent={Header}
-        ItemSeparatorComponent={<View style={menuStyles.saperator}></View>}
+        renderSectionHeader={renderSectionHeader}
+        renderItem={renderItem}
         ListFooterComponent={Footer}
-      ></FlatList>
+        ItemSeparatorComponent={Saperator}
+      ></SectionList>
     </View>
   );
 }
@@ -58,10 +74,16 @@ const menuStyles = StyleSheet.create({
   container: {
     flex: 0.75,
   },
-
+  sectionHeader: {
+    backgroundColor: "#223300",
+    padding: 20,
+    fontSize: 40,
+    color: "#F4CE14",
+  },
   headerText: {
     fontSize: 40,
     textAlign: "center",
+    alignSelf: "auto",
     color: "white",
   },
   menuItem: {
