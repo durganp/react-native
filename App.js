49d59f9faaa1a3eb552713f896f3fbfa33f3c780
Durga Nepal <durgain.np@gmail.com>
2023-01-05 17:07:36 +0900

    Navigation with drawer 'code is broken'

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
@@ -11,52 +11,13 @@ import Welcome from "./components/WelcomeScreen";
 import LoginScreen from "./components/LoginScreen";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function Logo() {
-  return (
-    <Image
-      style={{
-        height: 40,
-        width: 300,
-        resizeMode: "contain",
-        alignSelf: "center",
-      }}
-      source={require("./src/img/littleLemonHeader.png")}
-    />
-  );
-}
-
+const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={(props) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (props.route.name === "Welcome") {
-              iconName = focused
-                ? "ios-information-circle"
-                : "ios-information-circle-outline";
-            } else if (props.route.name === "Menu") {
-              iconName = "ios-list";
-            } else if (props.route.name === "Login") {
-              iconName = "ios-log-in";
-            } else if (props.route.name === "Feedback") {
-              iconName = "ios-mail-outline";
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "gray",
-        })}
-      >
-        <Tab.Screen name="Welcome" component={Welcome} />
-        <Tab.Screen name="Menu" component={MenuItems} />
-        <Tab.Screen name="Login" component={LoginScreen} />
-        <Tab.Screen name="Feedback" component={FeedBackForm} />
-      </Tab.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Welcome" component={Welcome} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
